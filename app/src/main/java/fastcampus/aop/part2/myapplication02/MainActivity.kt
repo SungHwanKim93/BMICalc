5f18d092e2342c20befb062fdf73e6deb34aa710
shkcr <shkcreate@gmail.com>
2024-08-28 17:36:32 +0900

    화면구성 커밋

---
+++ app/src/main/java/fastcampus/aop/part2/myapplication02/MainActivity.kt
@@ -0,0 +1,30 @@
+package fastcampus.aop.part2.myapplication02
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+        setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText)
+        val weightEditText = findViewById<EditText>(R.id.weightEditText)
+
+        val resultButton = findViewById<Button>(R.id.resultButton)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
